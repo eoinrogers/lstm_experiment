bbff12a5f65d61863e3702d6bda86f02be024ccf
Eoin Rogers <eoinrogers@gmail.com>
2019-08-27 16:50:53 +0100

    Update ground truth also

--- gen_links.py
+++ gen_links.py
@@ -11,9 +11,8 @@ def getargs():
     parser.add_argument('perplexity', help='Prototype for the path to the perplexity files', type=str)
     parser.add_argument('-w', '--window_length', help='Window length', type=int, default=30)
     parser.add_argument('-l', '--lookahead', help='Lookahead length', type=int, default=10)
-    parser.add_argument('-o', '--order', help='Ignore order when typing links', action='store_true')
     args = parser.parse_args()
-    return (args.dataset, args.input, args.output, args.word2id, args.perplexity, args.window_length, args.lookahead, args.order)
+    return (args.dataset, args.input, args.output, args.word2id, args.perplexity, args.window_length, args.lookahead)
 
 def get_most_likely(pvect, word2id, lookahead_offset, perplexity): 
     pvect = [item * perplexity for item in pvect[:len(word2id)]]
@@ -26,17 +25,7 @@ def print_progress(current, total, size=20):
     n = round((current / total) * size)
     sys.stdout.write('|{}{}|\r'.format('#' * n, ' ' * (size-n)))
 
-def update_type_labels(type_labels, one, two, order, contval):
-    v1 = '{} {}'.format(one, two)
-    v2 = '{} {}'.format(two, one)
-    if v1 not in type_labels: 
-        type_labels[v1] = 'new_event_{}'.format(contval)
-        if not order: 
-            type_labels[v2] = 'new_event_{}'.format(contval)
-        contval += 1
-    return type_labels, contval
-
-def process_single_lookahead(dataset, window_length, word2id_path, perplexity_path, incoming_path, outgoing_path, lookahead_length, type_labels, lookahead_offset, order, contval): 
+def process_single_lookahead(dataset, window_length, word2id_path, perplexity_path, incoming_path, outgoing_path, lookahead_length, lookahead_offset): 
     output = open(outgoing_path, 'w')
     word2id_dict = reader.readin_word2id(word2id_path)
     word2id = [None for item in range(len(word2id_dict))]
@@ -51,10 +40,8 @@ def process_single_lookahead(dataset, window_length, word2id_path, perplexity_pa
         lookahead = dataset[i+1:i+1+lookahead_length]
         j = i + lookahead_offset
         if j >= len(dataset) or dataset[j] != most_likely: continue
-        type_labels, contval = update_type_labels(type_labels, dataset[i], dataset[j], order, contval)
-        output.write('{} {} {} {}\n'.format(i, j, probability, type_labels['{} {}'.format(dataset[i], dataset[j])]))
+        output.write('{} {} {}\n'.format(i, j, probability))
     output.close()
-    return type_labels, contval
 
 def get_continue_value(dataset): 
     output = 0
@@ -74,18 +61,16 @@ def load_dataset(path):
     return output
 
 def main(): 
-    d, i, o, w2i, perp, wl, ll, order = getargs()
+    d, i, o, w2i, perp, wl, ll = getargs()
     n = 1
     incoming = i.format(n)
     outgoing = o.format(n)
     word2id = w2i.format(n)
     perplexity = perp.format(n)
-    type_labels = {}
     dataset = load_dataset(d)
-    contval = get_continue_value(dataset)
     while os.path.exists(incoming): 
         print(incoming)
-        type_labels, contval = process_single_lookahead(dataset, wl, word2id, perplexity, incoming, outgoing, ll, type_labels, n, order, contval)
+        process_single_lookahead(dataset, wl, word2id, perplexity, incoming, outgoing, ll, n)
         n += 1
         incoming = i.format(n)
         outgoing = o.format(n)
--- integrate_links.py
+++ integrate_links.py
@@ -1,18 +1,28 @@
-import gen_links, argparse, os
+import gen_links, argparse, os, math
 
 def getargs(): 
     parser = argparse.ArgumentParser('Combine links based on perplexity and probability measures.')
     parser.add_argument('dataset', help='Path to the dataset directory (note this should be a directory, not a file)', type=str)
     parser.add_argument('linkproto', help='Prototype for the files containing the candidate links written out by gen_links.py', type=str)
+    parser.add_argument('ground', help='Path to the ground truth file', type=str)
     parser.add_argument('destination', help='Path to a \'final\' linkset file that will contain the links chosen to be written into the new dataset', type=str)
     parser.add_argument('newdata', help='Path to the directory that the new dataset shold be written into', type=str)
+    parser.add_argument('newground', help='Path to write the new ground truth into', type=str)
     args = parser.parse_args()
-    return args.dataset, args.linkproto, args.destination, args.newdata
+    return args.dataset, args.linkproto, args.ground, args.destination, args.newdata, args.newground
+
+def load_ground_truth(path): 
+    handler = open(path, 'r')
+    for line in handler: 
+        line = line.strip()
+    handler.close()
+    line = [item.index('1') for item in line.split()]
+    return line
 
 def update_candidate_dict_for_single_file(path, candidates): 
     for line in open(path, 'r'): 
         line = line.strip().split()
-        if len(line) != 4: continue
+        if len(line) != 3: continue
         i, j, p = int(line[0]), int(line[1]), float(line[2])
         if i in candidates: 
             candidates[i].append((i, j, p))
@@ -62,7 +72,7 @@ def save_linkset(linkset, path):
         handler.write('{} {} {}\n'.format(i, j, p))
     handler.close()
 
-def build_new_dataset(dataset, linkset): 
+def build_new_dataset(dataset, linkset, ground_truth): 
     if len(linkset) == 0: return dataset[:]
     events = {}
     contval = 0
@@ -81,10 +91,13 @@ def build_new_dataset(dataset, linkset):
             link_type = events[contents_two]
         else: 
             link_type = 'new_event_{}'.format(contval)
+            events[contents_one] = link_type
             contval += 1
         output[i] = link_type
         output[j] = None
-    return [item for item in output if item != None]
+    print(events)
+    new_ground_truth = [item for item, corresponding_event in zip(ground_truth, output) if corresponding_event != None]
+    return [item for item in output if item != None], ground_truth
 
 def save_dataset(dataset, path, train_pc=.6, test_pc=.2, valid_pc=.2): 
     i = round(len(dataset) * train_pc)
@@ -100,9 +113,21 @@ def save_dataset(dataset, path, train_pc=.6, test_pc=.2, valid_pc=.2):
         handler.write(strings[i])
         handler.close()
 
+def save_ground_truth(ground_truth, destination): 
+    mkgrdstr = lambda item, length: ''.join(['1' if i == item else '0' for i in range(length)])
+    length = max(ground_truth) + 1
+    handler = open(destination, 'w')
+    guide = ''.join(['{} '.format(i) for i in range(length)])[:-1]
+    handler.write('{}\n'.format(guide))
+    ground_strings = [mkgrdstr(i, length) for i in ground_truth]
+    output = ''.join(['{} '.format(i) for i in ground_strings])[:-1]
+    handler.write(output)
+    handler.close()
+
 def main(): 
-    datadir, linkproto, destpath, newdir = getargs()
+    datadir, linkproto, grdpath, destpath, newdir, newgrd = getargs()
     dataset = gen_links.load_dataset(datadir)
+    ground_truth = load_ground_truth(grdpath)
     candidates = {}
     n = 1
     linkset_path = linkproto.format(n)
@@ -114,8 +139,9 @@ def main():
     print('Done!')
     linkset = build_final_linkset(candidates)
     save_linkset(linkset, destpath)
-    new_dataset = build_new_dataset(dataset, linkset)
+    new_dataset, new_ground_truth = build_new_dataset(dataset, linkset, ground_truth)
     save_dataset(new_dataset, newdir)
+    save_ground_truth(new_ground_truth, newgrd)
 
 if __name__ == '__main__':
     main()
